c936c3d6f91f8e7bb8858d41279b4408346dc1ac
LutSerg <asteroster@gmail.com>
2022-04-30 22:58:02 +0300

    added annotated tests

---
+++ src/test/java/gmail/asteroster/tests/TestWithStepAnnotation.java
@@ -0,0 +1,24 @@
+package gmail.asteroster.tests;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestWithStepAnnotation {
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final String ISSUENAME = "Listeners NamedBy";
+
+    @Test
+    @DisplayName("Тест с использованием аннотации Step")
+    void annotationTest () {
+    SelenideLogger.addListener("allure", new AllureSelenide());
+    WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.openIssuesTab();
+        steps.shouldSeeIssueWithName(ISSUENAME);
+    }
+}
